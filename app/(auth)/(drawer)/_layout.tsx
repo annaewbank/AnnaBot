2b52206147093613aedb9b3bf4279f202fa92a4e
Anna Ewbank <Ewbank145@gmail.com>
2024-05-28 16:38:26 +0100

    feat: drawer component set-up

---
+++ app/(auth)/(drawer)/_layout.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import { Drawer } from 'expo-router/drawer';
+
+// This file defines the drawer content
+
+const Layout = () => {
+  return <Drawer></Drawer>;
+};
+
+export default Layout;
