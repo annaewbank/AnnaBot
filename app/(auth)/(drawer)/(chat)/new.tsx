b22bd26093c622841644a4b0dba2e4ec3d7099d4
Anna Ewbank <Ewbank145@gmail.com>
2024-05-29 10:06:57 +0100

    feat: zeego dropdown initial set-up

--- app/(auth)/(drawer)/(chat)/new.tsx
+++ app/(auth)/(drawer)/(chat)/new.tsx
@@ -1,4 +1,6 @@
+import { defaultStyles } from '@/constants/Styles';
 import { useAuth } from '@clerk/clerk-expo';
+import { Stack } from 'expo-router';
 import { View, Button } from 'react-native';
 
 // Everything inside (auth) is protected by Clerk
@@ -8,7 +10,9 @@ const Page = () => {
   const { signOut } = useAuth();
 
   return (
-    <View>
+    <View style={defaultStyles.pageContainer}>
+      {/* Change header on page instead of on layout */}
+      <Stack.Screen options={{ title: 'Chat' }} />
       <Button title="Sign out" onPress={() => signOut()} />
     </View>
   );
---
+++ components/HeaderDropDown.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const HeaderDropDown = () => {
+  return (
+    <View>
+      <Text>HeaderDropDown</Text>
+    </View>
+  );
+};
+
+export default HeaderDropDown;
