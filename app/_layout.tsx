a56f92015ca0f89af82c7af56c5dc2b7abbbe051
Anna Ewbank <Ewbank145@gmail.com>
2024-05-30 21:00:19 +0100

    docs: comments for reference

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -65,6 +65,8 @@ const InitialLayout = () => {
 
     const inAuthGroup = segments[0] === '(auth)';
 
+    console.log('segments', segments);
+    // e.g. segments: ["(auth)", "(drawer)", "(chat)", "new"]
     console.log('isSignedIn', isSignedIn);
     console.log('inAuthGroup', inAuthGroup);
 
